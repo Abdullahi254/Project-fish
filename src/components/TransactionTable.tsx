3c94c7a89cdc55aef96d1f6f956cf3a1c649a591
Abdullahi254 <abdush504@gmail.com>
2023-07-01 20:34:51 +0300

    added row for total credit and debit

--- src/components/TransactionTable.tsx
+++ src/components/TransactionTable.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React,{useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { RxReset } from "react-icons/rx"
 import { useRouter } from 'next/navigation';
 type Props = {
@@ -7,7 +7,7 @@ type Props = {
     endDate: string | string[] | undefined
 }
 
-const TransactionTable = ({startDate, endDate}: Props) => {
+const TransactionTable = ({ startDate, endDate }: Props) => {
     const [toogle, setToogle] = useState<boolean>()
     const firstDateRef = useRef<HTMLInputElement>(null)
     const secondDateRef = useRef<HTMLInputElement>(null)
@@ -28,8 +28,8 @@ const TransactionTable = ({startDate, endDate}: Props) => {
         router.push("/")
     }
 
-  return (
-    <>
+    return (
+        <>
             <div className='w-full mb-2 space-x-4 py-2 px-2 md:px-8 flex items-center justify-start md:justify-end'>
                 <span className='text-sm text-gray-500'>From:</span>
                 <input type='date' ref={firstDateRef} className='text-sm text-gray-600' onChange={handleDateChange} />
@@ -48,31 +48,45 @@ const TransactionTable = ({startDate, endDate}: Props) => {
             <table className="w-full text-sm text-left text-gray-500">
                 <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
                     <tr>
+                        <th scope="col" className="px-6 py-3">NO</th>
                         <th scope="col" className="px-6 py-3">Date</th>
-                        <th scope="col" className="px-6 py-3">Detail</th>
-                        <th scope="col" className="px-6 py-3">Debit</th>
-                        <th scope="col" className="px-6 py-3">Credit</th>
-                        <th scope="col" className="px-6 py-3">Balance</th>
+                        <th scope="col" className="px-6 py-3">Debit(KSH)</th>
+                        <th scope="col" className="px-6 py-3">Credit(KSH)</th>
+                        <th scope="col" className="px-6 py-3">Debt Balance(KSH)</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        [1,2,3,4,5,6].map((data) => <React.Fragment key={data}>
+                        [1, 2, 3, 4, 5, 6].map((data) => <React.Fragment key={data}>
                             <tr className='bg-white border-b' >
+                                <td className="px-6 py-4">{data}</td>
                                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
                                     12/13/2020
                                 </th>
-                                <td className="px-6 py-4">Credit</td>
-                                <td className="px-6 py-4">0.00</td>
+                                <td className="px-6 py-4"></td>
                                 <td className="px-6 py-4">1200.00</td>
                                 <td className="px-6 py-4">1200.00</td>
                             </tr>
                         </React.Fragment>)
                     }
+                    <tr className='bg-white' >
+                        <td className="px-6 py-4"></td>
+                        <td className="px-6 py-4 font-bold text-black">TOTAL:</td>
+                        <td className="px-6 py-4">0.00</td>
+                        <td className="px-6 py-4">7200.00</td>
+                        <td className="px-6 py-4 font-bold text-black">7200.00</td>
+                    </tr>
+                    <tr className='bg-white' >
+                        <td className="px-6 py-4"></td>
+                        <td className="px-6 py-4"></td>
+                        <td className="px-6 py-4"></td>
+                        <td className="px-6 py-4"></td>
+                        <td className="px-6 py-4"><button className='text-blue-600 hover:text-indigo-900 text-sm'>Full table</button></td>
+                    </tr>
                 </tbody>
             </table>
         </>
-  )
+    )
 }
 
 export default TransactionTable
