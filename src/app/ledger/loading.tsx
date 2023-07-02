c211765dd17a69e7b4f1e8ce67d581ae80b1adef
Abdullahi254 <abdush504@gmail.com>
2023-07-02 14:47:18 +0300

    added loading page in ledger page

---
+++ src/app/ledger/loading.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+function Loading({ }: Props) {
+    return (
+        <div className="mt-14 w-full">
+            <div className="max-w-7xl mx-auto overflow-x-auto">
+                <div className='w-full px-4 bg-white flex flex-col items-center justify-center animate-pulse py-4'>
+                    {
+                        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(data => <div key={data} className="h-[40px] bg-gray-200 rounded-lg w-full mb-4"></div>)
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
