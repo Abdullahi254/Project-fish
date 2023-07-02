29c7d84ce2433e755d10e362a352f9a42fd28978
Abdullahi254 <abdush504@gmail.com>
2023-07-02 13:42:37 +0300

    added searching using search param logic

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,22 +1,76 @@
-import React from 'react'
-import {AiOutlineSearch as SearchIcon} from "react-icons/ai"
+'use client';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useCallback, useEffect, useState, useTransition } from 'react'
+import { AiOutlineSearch as SearchIcon } from "react-icons/ai"
+import { AiOutlineLoading3Quarters as Spinner } from 'react-icons/ai'
 type Props = {}
 
+
 const SearchForm = (props: Props) => {
+    const [inputValue, setInputValue] = useState<string>('')
+    const [debouncedValue, setDebouncedValue] = useState<string>('')
+    const [mounted, setMounted] = useState<boolean>(false)
+    const [isPending, startTransition] = useTransition()
+    const router = useRouter()
+    const pathname = usePathname()
+
+    const handleSearchParams = useCallback((value: string) => {
+        let params = new URLSearchParams(window.location.search)
+        if (value.length > 0) {
+            params.set("search", value)
+        } else {
+            params.delete("search")
+        }
+        console.log(params.toString())
+        startTransition(() => {
+            router.replace(`${pathname}?${params.toString()}`)
+        })
+    }, [pathname, router])
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search)
+        const searchQuery = params.get("search") ?? ''
+        setInputValue(searchQuery)
+    }, [])
+
+    useEffect(() => {
+        if (debouncedValue.length > 0 && !mounted) {
+            setMounted(true)
+        }
+    }, [debouncedValue, mounted])
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedValue(inputValue)
+        }, 500)
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [inputValue])
+    useEffect(() => {
+        if (mounted) {
+            handleSearchParams(debouncedValue)
+        }
+    }, [debouncedValue, handleSearchParams, mounted])
+
     return (
-        <form className="flex items-center">
-            <label htmlFor="simple-search" className="sr-only">Search</label>
-            <div className="relative w-full">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <SearchIcon className='text-lg md:text-xl text-gray-500'/>
+        <>
+            <form className="flex items-center" onSubmit={(e)=>e.preventDefault()}>
+                <label htmlFor="simple-search" className="sr-only">Search</label>
+                <div className="relative w-full">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <SearchIcon className='text-lg md:text-xl text-gray-500' />
+                    </div>
+                    <input value={inputValue} onChange={(e) => setInputValue(e.target.value)} type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full pl-10 p-2.5" placeholder="Search" required />
                 </div>
-                <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full pl-10 p-2.5" placeholder="Search" required />
-            </div>
-            <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-gray-900 rounded-lg border border-gray-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300">
-                <SearchIcon className='text-lg md:text-xl'/>
-                <span className="sr-only">Search</span>
-            </button>
-        </form>
+            </form>
+            { isPending &&
+                <div className='w-full flex justify-center pt-2'>
+                    <Spinner className='animate-spin'/>
+                </div>
+            }
+        </>
+
     )
 }
 
